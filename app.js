cd678195c8b1e6f6ba2c929d603a0818012903bf
Jahanzaib Channar <jahanzaibchannar954@gmail.com>
2026-08-13 23:21:16 +0500

    make small Changes

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 console.log("working")
 
+let url = "https://dummyjson.com/users";
+
+async function Checkapi() {
+    try {
+        const res = await fetch(url);
+
+        const text = await res.json();
+
+        console.log(text)
+
+    } catch (error) {
+        console.log(error)
+
+    }
+
+
+}
+
+Checkapi()
